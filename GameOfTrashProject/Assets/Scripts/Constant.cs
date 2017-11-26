037d34b8c9d0567a8744676db317fa1a482fbb5e
Yolly Fe Oppura <yollyfer18opp@gmail.com>
2017-11-26 15:45:11 +0800

    Registration connected to App42

---
+++ GameOfTrashProject/Assets/Scripts/Constant.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using com.shephertz.app42.paas.sdk.csharp;
+using com.shephertz.app42.paas.sdk.csharp.game;
+using com.shephertz.app42.paas.sdk.csharp.user;
+
+public class Constant : MonoBehaviour
+{
+    public string apiKey = "6d3315644df7ab693ec31e305146d091e5bd99ef48d5b059d9564f47506b7cd5";
+    public string secretKey = "aa6ca34fa6b87e672c7e5a3c5d396863c1b99d8f28fa2c476c75e3964b03408a";
+    public string gameName = "GOT";
+    public string dbName = "GOTDB";
+}
---
+++ GameOfTrashProject/Assets/Scripts/RegisterScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+using com.shephertz.app42.paas.sdk.csharp;
+using com.shephertz.app42.paas.sdk.csharp.user;
+
+public class RegisterScript : MonoBehaviour
+{
+
+    string userField, passField, confPassField, emailField;
+    InputField username, pass, confPass, email;
+
+    void Start()
+    {
+        Debug.Log("hey");
+        username = GameObject.Find("username").GetComponent<InputField>();
+        pass = GameObject.Find("pass").GetComponent<InputField>();
+        confPass = GameObject.Find("confpass").GetComponent<InputField>();
+        email = GameObject.Find("email").GetComponent<InputField>();
+    }
+
+    public bool fieldCheck()
+    {
+        bool flag = false;
+
+        userField = username.text;
+        passField = pass.text;
+        confPassField = confPass.text;
+        emailField = email.text;
+
+        if (!userField.Equals("") && !passField.Equals("") && !confPassField.Equals("") && !emailField.Equals(""))
+        {
+            flag = true;
+            Debug.Log("q");
+        }
+
+        if (passField.Equals(confPassField)) flag = true;
+
+        return flag;
+    }
+
+    public void registerBtn()
+    {
+        Debug.Log("yey");
+        if (fieldCheck())
+        {
+            Constant cons = new Constant();
+            App42API.Initialize(cons.apiKey, cons.secretKey);
+            UserService userService = App42API.BuildUserService();
+            try
+            {
+                userService.CreateUser(userField, passField, emailField, new UserResponse());
+            }
+            catch (App42Exception e)
+            {
+                
+            };
+            App42Log.SetDebug(true);
+        }
+    }
+
+}
