130777fea3b3730804672848dbde38ddf9b50bd3
Shiela Mae Bobadilla <shielamaebobadilla@gmail.com>
2017-12-13 02:25:36 +0800

    Change level background

---
+++ Assets/Scripts/MapScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using com.shephertz.app42.paas.sdk.csharp;
+using UnityEngine.UI;
+using com.shephertz.app42.paas.sdk.csharp.storage;
+using com.shephertz.app42.paas.sdk.csharp.user;
+using System;
+
+public class MapScript: MonoBehaviour {
+
+    public int level;
+    public Sprite level1;
+    public Sprite level2;
+    public Sprite level3;
+    public Sprite level4;
+    public Sprite level5;
+    public GameObject bg;
+
+    MapScript (int level)
+    {
+        this.level = level;
+    }
+
+    void Start()
+    {
+        switch (level)
+        {
+            case 1: bg.GetComponent<Image>().sprite = level1; break;
+            case 2: bg.GetComponent<Image>().sprite = level2; break;
+            case 3: bg.GetComponent<Image>().sprite = level3; break;
+            case 4: bg.GetComponent<Image>().sprite = level4; break;
+            case 5: bg.GetComponent<Image>().sprite = level5; break;
+        }
+    }
+
+}
