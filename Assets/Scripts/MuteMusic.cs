4d13a05aed458fb6850bd869012ff462e00ddbbf
Yolly Fe Oppura <yollyfer18opp@gmail.com>
2018-02-04 18:13:19 +0800

    Fixed working mute of music and sound Effects

---
+++ Assets/Scripts/MuteMusic.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuteMusic : MonoBehaviour {
+
+    bool isplay = false;
+
+    public void muteMusic()
+    {
+        AudioSource music = GameObject.Find("MusicOne").GetComponent<AudioSource>();
+        music.ignoreListenerVolume = true;
+
+        if (!isplay)
+        {
+            music.Pause();
+            isplay = true;
+
+        }
+        else
+        {
+            music.UnPause();
+            isplay = false;
+        }
+    }
+
+}
