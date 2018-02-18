629b72c542b62c80f3055c72166655726af7239a
Shiela Mae Bobadilla <shielamaebobadilla@gmail.com>
2018-02-19 05:53:46 +0800

    Trash Seg drag and drop. UI for levels

---
+++ Assets/Scripts/TrashDropHolder.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class TrashDropHolder : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+
+    private bool mouseDown = false;
+    private Vector3 startMousePos;
+    private Vector3 startPos;
+
+
+    public void OnPointerDown(PointerEventData ped)
+    {
+        mouseDown = true;
+        startPos = transform.position;
+        startMousePos = Input.mousePosition;
+    }
+
+    public void OnPointerUp(PointerEventData ped)
+    {
+        mouseDown = false;
+    }
+
+
+    void Update()
+    {
+        if (mouseDown)
+        {
+            Vector3 currentPos = Input.mousePosition;
+            Vector3 diff = currentPos - startMousePos;
+            Vector3 pos = startPos + diff;
+            transform.position = pos;
+        }
+    }
+
+}
--- Assets/Scripts/TrashDropScript.cs
+++ Assets/Scripts/TrashDropScript.cs
@@ -8,34 +8,25 @@ using System;
 
 public class TrashDropScript : MonoBehaviour {
 
-    Constant c;
-    public Image img;
-
-    GameObject obj;
-    // public float speed = 10F;
-    // Use this for initialization
-
+    public GameObject[] obj;
     List<TrashData> trash;
-
-    // Update is called once per frame
-    void Update () {
-
-		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) {
-			Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;            
-			transform.Translate(touchDeltaPosition.x, touchDeltaPosition.y, 0);
-		}
-	}
-
     public string url = "http://cdn.shephertz.com/repository/files/6d3315644df7ab693ec31e305146d091e5bd99ef48d5b059d9564f47506b7cd5/5b40e645c8d6827394d41ddaf53aeb3e452b29b5/tire.png";
 
-    IEnumerator Start()
+    private void Start()
     {
-        obj = new GameObject("tra");
-        obj.AddComponent<Image>();
-        WWW www = new WWW(url);
-        yield return www;
-        obj.GetComponent<Image>().sprite = Sprite.Create(www.texture, new Rect(0, 0, www.texture.width, www.texture.height), new Vector2(0, 0));
-        Instantiate(obj);
+        
+    }
+    IEnumerator DeployTrash()
+    {
+        for(int i = 0; i < obj.Length; i++)
+        {
+            WWW www = new WWW(url);
+            yield return www;
+
+            if (www.isDone) obj[i].SetActive(true);
+            obj[i].GetComponent<Image>().sprite = Sprite.Create(www.texture, new Rect(0, 0, www.texture.width, www.texture.height), new Vector2(0, 0));
+            Instantiate(obj[i]);           
+        }        
     }
 
 }
