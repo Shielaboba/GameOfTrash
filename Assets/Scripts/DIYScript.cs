18689bbe38c1bd9e63b24f02962ff2b52a53dac0
Niño Sasan <oninsasan1208@gmail.com>
2018-02-22 23:27:47 +0800

    DIY DONE!

--- Assets/Scripts/DIYScript.cs
+++ Assets/Scripts/DIYScript.cs
@@ -8,7 +8,8 @@ using UnityEngine.UI;
 using System;
 using LitJson;
 
-public class DIYScript : MonoBehaviour {
+public class DIYScript : MonoBehaviour
+{
 
     // Use this for initialization
 
@@ -25,11 +26,6 @@ public class DIYScript : MonoBehaviour {
         storageService.FindDocumentsByQuery(cons.dbName, "DIYFile", query, new DIYResponse());
     }
 
-    public void GotoProcedures()
-    {                
-        SceneManager.LoadScene("DIY_procedure");
-    }
-   
 }
 
 internal class DIYResponse : App42CallBack
@@ -38,81 +34,34 @@ internal class DIYResponse : App42CallBack
     public Text prepareText;
     public Text name;
 
-    DIYTrashData[] diy;
+    List<DIYTrashData> diy;
 
     public void OnSuccess(object response)
     {
         Storage storage = (Storage)response;
         IList<Storage.JSONDocument> jsonDocList = storage.GetJsonDocList();
-        diy = new DIYTrashData[2];        
+        diy = new List<DIYTrashData>();
 
         for (int i = 0; i < jsonDocList.Count; i++)
         {
-            diy[i] = JsonUtility.FromJson<DIYTrashData>(jsonDocList[i].GetJsonDoc());
-            
+            diy.Add(JsonUtility.FromJson<DIYTrashData>(jsonDocList[i].GetJsonDoc()));
+
         }
 
-        Scene scene = SceneManager.GetActiveScene();
-
-        if (scene.name.Equals("DIY"))
-        {            
-            GameObject.Find("name_diy1").GetComponent<Text>().text = diy[0].DIYCraftName;
-            GameObject.Find("name_diy2").GetComponent<Text>().text = diy[1].DIYCraftName;
-            IEnumerator callEnum = FindImage();
-            while(callEnum.MoveNext())
-            {
-
-            }
-            
-                        
-        }
+        TrashRandomManager.GetInstance().SetDIYTrash(diy);
     }
 
-   IEnumerator FindImage()
-    {
-        Dictionary<string, string> headers = new Dictionary<string, string>();
-        headers.Add("Ocp-Apim-Subscription-Key", "1b4d9849f0e14b3a815c56c5958832be");
 
-        for(int i=0; i < diy.Length; i++)
-        {
-            string craft = diy[i].DIYCraftName.Replace(' ', '+').ToLower();
-            var url = "https://api.cognitive.microsoft.com/bing/v7.0/search?q=" + craft + "&answerCount=1&responseFilter=images&mkt=en-us";
-            
-            using (WWW www = new WWW(url, null, headers))
-            {
-                yield return www;
-
-                if (string.IsNullOrEmpty(www.error))
-                {
-                        JsonData picUrl = JsonMapper.ToObject(www.text);
-
-                        Debug.Log("IN");
-                        Texture2D tex = new Texture2D(621, 397, TextureFormat.DXT1, false);
-                        using (WWW w = new WWW(picUrl["images"]["value"][i]["thumbnailUrl"].GetString()))
-                        {
-                            yield return w;
-                            w.LoadImageIntoTexture(tex);
-                            GameObject.Find("BtnSample" + (i + 1)).GetComponent<Image>().material.mainTexture = tex;
-                        }
-                 
-                }
-                else
-                {
-                    Debug.Log("Error: " + www.error);
-                }
-            }
-        }        
-    }
 
     public void DisplayCraftDetails()
     {
         name = GameObject.Find("name_diy").GetComponent<Text>();
         prepareText = GameObject.Find("Procedure").GetComponent<Text>();
         procedure = GameObject.Find("prepareText").GetComponent<Text>();
-    }    
+    }
 
     public void OnException(Exception ex)
     {
         throw new NotImplementedException();
-    }   
+    }
 }
