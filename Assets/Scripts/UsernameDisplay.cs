a747580b4498c5a2c96ec5a8edf3b242ddee649f
Yolly Fe Oppura <yollyfer18opp@gmail.com>
2018-02-28 01:01:19 +0800

    Username Display in Settings Modal

---
+++ Assets/Scripts/UsernameDisplay.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UsernameDisplay : MonoBehaviour {
+
+    Text username;
+    
+    void Start () {
+        String userName = PlayerPrefs.GetString("username");
+        username = GetComponent<Text>();
+        username.text = userName;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
