d376f79b05f96084a31ee1a470219c60a2053cfc
Shiela Mae Bobadilla <shielamaebobadilla@gmail.com>
2018-03-06 09:33:58 +0800

    Fix Tutorial Toggle btn

--- Assets/Scripts/TutorialScript.cs
+++ Assets/Scripts/TutorialScript.cs
@@ -6,30 +6,46 @@ using UnityEngine.SceneManagement;
 public class TutorialScript : MonoBehaviour {
 
     GameObject tutorialPanel;
-    int tutorialNo;
+    int tutorialNo, level;
     static int currNo = 1;
     string scene;
+    static bool tutorial;
 
     // Use this for initialization
-    void Start () {        
+    void Start() {
 
         tutorialPanel = GameObject.Find("TutorialPanel");
         scene = SceneManager.GetActiveScene().name;
+        level = PlayerManager.GetInstance().GetPlayer().PlayerGameLvlNo;
+
+        if ((level != 1 && !tutorial) || currNo == 5)
+        {
+            currNo = 0;
+            tutorial = false;
+        }        
+
         if (scene.Equals("map")) tutorialNo = 1;
         else if (scene.Equals("trash_menu")) tutorialNo = 2;
         else if (scene.Equals("trash_search")) tutorialNo = 3;
         else if (scene.Equals("trash_seg")) tutorialNo = 4;        
 
-        if (PlayerManager.GetInstance().GetPlayer().PlayerGameLvlNo != 1 || currNo != tutorialNo)
+        if (currNo != tutorialNo)
         {
             tutorialPanel.SetActive(false);
         }
+        print("c: " + currNo);
+        print("t: " + tutorialNo);
     }
 	
 	public void NextTutorial()
     {
         currNo++;
-        PlayerManager.GetInstance().SetTutorial(currNo);
+    }
+
+    public void BeginTutorial()
+    {
+        currNo = 1;
+        tutorial = true;
     }
 
 }
