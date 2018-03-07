60bf6425d353b4687ce27eb30cbe26f0e8488612
Summer Calva <summerbacallacalva@gmail.com>
2018-03-07 11:03:29 +0800

    qwerty

---
+++ Assets/Scripts/CurrentTimeResponse.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using com.shephertz.app42.paas.sdk.csharp;
+using com.shephertz.app42.paas.sdk.csharp.timer;
+using System;
+
+public class CurrentTimeResponse : App42CallBack
+{
+    
+    public void OnSuccess(object response)
+    {
+        Timer timer = (Timer)response;
+        
+        PlayerPrefs.SetString("CurrentTime", timer.GetCurrentTime().ToString());
+    }
+
+    public void OnException(Exception ex)
+    {
+        throw new NotImplementedException();
+    }    
+}
